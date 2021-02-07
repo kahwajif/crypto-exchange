6dd47d7f8bba0f95968ae23e0971f53668c64716
Feras Kahwaji <feraskahwaji@hotmail.com>
2021-02-07 00:15:08 -0500

    FInished Dogecoin card

--- crypto-exchange/src/app/home/crypto-cards/doge-card/doge-card.component.ts
+++ crypto-exchange/src/app/home/crypto-cards/doge-card/doge-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-doge-card',
@@ -7,9 +8,124 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DogeCardComponent implements OnInit {
 
-  constructor() { }
+  crypto: any;
+  USD: any;
 
-  ngOnInit(): void {
+ 
+  //for buy/sell inputs
+  USD1: number;;
+  DOGE1: number;;
+  USD2: number;;
+  DOGE2: number;;
+
+  constructor(private _data: DataService){
+  }
+
+  ngOnInit(){
+    //get DOGE current price data
+    this._data.getPricesInUSD()
+      .subscribe(res=>{
+        this.crypto = res['DOGE'].USD;
+    });
+    // get USD in DOGE
+    this._data.getPriceInCrypto()
+      .subscribe(res=>{
+        this.USD = res['DOGE'];
+    });
+    this._data.dogeChart(1);
+  }
+
+//converts USD to ETH and vice versa
+  get result1DOGE(){
+    return Number(this.USD1*this.USD);
+  }
+  get result1USD(){
+    return Number(this.DOGE1*this.crypto);
+  }
+  get result2DOGE(){
+    return Number(this.USD2*this.crypto);
+  }
+  get result2USD(){
+    return Number(this.DOGE2*this.USD);
+  }
+
+  check1: boolean;
+  check2: boolean;
+  checkAmount1(){
+    this.checkNegative();
+    if(this.USD1 < 0 || this.DOGE1 < 0){
+      this.USD1 = 0;
+      this.DOGE1 = 0
+    }
+    else if(this.USD1 > 100000 || this.DOGE1 > 99){
+      this.check2 = true;
+      this.check1 = false;
+    }
+    else if(this.USD1 || this.DOGE1){
+      this.check1 = true;
+      this.check2 = false;
+    }
+    else
+      return 0;
+    return 1;
+  }
+
+  check3: boolean;
+  check4: boolean;
+  checkAmount2(){
+    this.checkNegative();
+    if(this.USD2 > 100000 || this.DOGE2 > 99){
+      this.check4 = true;
+      this.check3 = false;
+    }
+    else if(this.USD2 || this.DOGE2){
+      this.check3 = true;
+      this.check4 = false;
+    }
+    else
+      return 0;
+    return 1;
+  }
+
+  checkNegative(){
+    var a = document.getElementById('defaultInput');
+    var b = document.getElementById('buyInput');
+    var c = document.getElementById('sellInput');
+    var d = document.getElementById('defaultInput2');
+
+    a.onkeydown = function(e) {
+      if(!((e.keyCode > 95 && e.keyCode < 106)
+        || (e.keyCode > 47 && e.keyCode < 58)
+        || e.keyCode == 8)) {
+          return false;
+      }
+    }
+    b.onkeydown = function(e) {
+      if(!((e.keyCode > 95 && e.keyCode < 106)
+        || (e.keyCode > 47 && e.keyCode < 58)
+        || e.keyCode == 8)) {
+          return false;
+      }
+    }
+    c.onkeydown = function(e) {
+      if(!((e.keyCode > 95 && e.keyCode < 106)
+        || (e.keyCode > 47 && e.keyCode < 58)
+        || e.keyCode == 8)) {
+          return false;
+      }
+    }
+    d.onkeydown = function(e) {
+      if(!((e.keyCode > 95 && e.keyCode < 106)
+        || (e.keyCode > 47 && e.keyCode < 58)
+        || e.keyCode == 8)) {
+          return false;
+      }
+    }
+  }
+
+  changeData(x){
+    
+    this._data.dogeChart(x);
   }
 
 }
--- crypto-exchange/src/app/services/data.service.ts
+++ crypto-exchange/src/app/services/data.service.ts
@@ -16,12 +16,12 @@ export class DataService {
   }
   //price of ETH and BTC in USD
   getPricesInUSD(){
-    return this._http.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD&api_key='+API_KEY)
+    return this._http.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,DOGE&tsyms=USD&api_key='+API_KEY)
     .pipe(map(result => this.result = result));
   }
   //price of USD in ETH and BTC
   getPriceInCrypto(){
-    return this._http.get('https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=BTC,ETH&api_key='+ API_KEY)
+    return this._http.get('https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=BTC,ETH,DOGE&api_key='+ API_KEY)
     .pipe(map(result => this.result = result));
   }
 
@@ -49,7 +49,7 @@ export class DataService {
     let monthHistoricTime = [];
     let yearHistoricTime = [];
     let yearHistoricY = [];
-    const res = await fetch('https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=1825&api_key=621def888ea82cbdd98c3ba9f10fbd487e222c0663c524f468b1c3495192cb89');
+    const res = await fetch('https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=1825&api_key='+API_KEY);
     const data = await res.json();
     historicData = data.Data.Data;
     //5 years
@@ -180,9 +180,9 @@ export class DataService {
     }
     updateChart(x);
   }
-//===============================================BTC END===================================================
-//===============================================ETH START=================================================
-async getETH(){
+ //===============================================BTC END===================================================
+ //===============================================ETH START=================================================
+ async getETH(){
   let historicData;
   let historicX = [];
   let historicY = [];
@@ -192,7 +192,7 @@ async getETH(){
   let monthHistoricTime = [];
   let yearHistoricTime = [];
   let yearHistoricY = [];
-  const res = await fetch('https://min-api.cryptocompare.com/data/v2/histoday?fsym=ETH&tsym=USD&limit=1825&api_key=621def888ea82cbdd98c3ba9f10fbd487e222c0663c524f468b1c3495192cb89');
+  const res = await fetch('https://min-api.cryptocompare.com/data/v2/histoday?fsym=ETH&tsym=USD&limit=1825&api_key='+API_KEY);
   const data = await res.json();
   historicData = data.Data.Data;
   //5 years
@@ -225,9 +225,9 @@ async getETH(){
   }
 
   return {monthHistoricTime, monthHistoricY, historicY, historicYears, yearHistoricTime, yearHistoricY};
-}
+ } 
 
-async ethereumChart(x){
+ async ethereumChart(x){
   const data = await this.getETH();
   let currentTime, currentY;
 
@@ -322,9 +322,150 @@ async ethereumChart(x){
     chart.update();
   }
   updateChart(x);
-}
-//===============================================ETH END===================================================
-//===============================================DOGE START================================================
+ }
+ //===============================================ETH END===================================================
+ //===============================================DOGE START================================================
+ async getDOGE(){
+  let historicData;
+  let historicX = [];
+  let historicY = [];
+  let historicTime = [];
+  let historicYears = [];
+  let monthHistoricY =[];
+  let monthHistoricTime = [];
+  let yearHistoricTime = [];
+  let yearHistoricY = [];
+  const res = await fetch('https://min-api.cryptocompare.com/data/v2/histoday?fsym=DOGE&tsym=USD&limit=1825&api_key='+API_KEY);
+  const data = await res.json();
+  historicData = data.Data.Data;
+  //5 years
+  for (let value of historicData){
+    historicX.push(value.time);
+    historicY.push(value.close);
+  }
+  for(let UNIX_time of historicX){
+    var s = this.convertTime(UNIX_time);
+    historicTime.push(s);
+  }
+  //5 year time
+  for(let i=0;i<1825;i++){
+    var removeDay = historicTime[i].substring(historicTime[i].indexOf(" "), historicTime[i].lastIndexOf(" "));
+    var corrected = historicTime[i].replace(removeDay, "");
+    historicYears.push(corrected);
+  }
+  //1 year
+  for(let i=0;i<365;i++){
+    yearHistoricY.push(historicY[1460+i])
+    var removeDay = historicTime[1460+i].substring(historicTime[1460+i].indexOf(" "), historicTime[1460+i].lastIndexOf(" "));
+    var corrected = historicTime[1460+i].replace(removeDay, "");
+    yearHistoricTime.push(corrected);
+  }
 
-//===============================================DOGE END==================================================
-}
+  //1 month
+  for(let i=0;i<30;i++){
+    monthHistoricY.push(historicY[1795+i])
+    monthHistoricTime.push(historicTime[1795+i].substring(0, historicTime[1795+i].lastIndexOf(" ")));
+  }
+
+  return {monthHistoricTime, monthHistoricY, historicY, historicYears, yearHistoricTime, yearHistoricY};
+  }
+
+ async dogeChart(x){
+  const data = await this.getDOGE();
+  let currentTime, currentY;
+
+  var chart = new Chart('dogeChart', {
+    type: "line",
+    data: {
+      labels: currentTime,
+      datasets: [
+        {
+          label: "Dogecoin ",
+          pointRadius: 3,
+          pointBorderWidth: 1,
+          pointBackgroundColor: "#f4a460",
+          borderWidth: '1',
+          borderColor: "#f4a460",
+          fontColor: 'white',
+          defaultFontSize: 20,
+          data: currentY
+        }
+      ]
+    },
+    options: {
+      elements: {
+        point: {
+          radius: 0,
+          hitRadius: 5,
+          hoverRadius: 5
+        }
+      },
+      legend: {
+        display: false
+      },
+      scales: {
+        yAxes: [
+          {
+            display: true,
+            position: 'right',
+            ticks: {
+              beginAtZero: false,
+              fontSize: 15,
+              fontColor: 'white',
+              callback: function(value, index, values) {
+                return '$' + value;
+              }
+            },
+          }
+        ],
+        xAxes: [
+          {
+            gridLines:{
+              display: false
+            },
+            ticks: {
+              autoSkipPadding: 20,
+              fontColor: 'white',
+              fontSize: 15,
+              maxRotation: 0,
+              minRotation: 0,
+              
+            }
+          }
+        ],
+      },
+      layout: {
+        padding: {
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0
+        }
+      }
+    }
+  });
+  
+  function updateChart(x){
+    if(x == 1){ //month
+      
+      chart.data.datasets[0].data = data.monthHistoricY;
+      chart.data.labels = data.monthHistoricTime;
+    }
+    else if(x == 2){ //1 year
+      chart.data.datasets[0].data = data.yearHistoricY;
+      chart.data.labels = data.yearHistoricTime;
+      chart.data.datasets[0].pointRadius = 0;
+    }
+    else if(x == 3){ // 5 years
+
+      chart.data.datasets[0].data = data.historicY;
+      chart.data.labels = data.historicYears;
+      chart.data.datasets[0].pointRadius = 0;
+    }
+    chart.update();
+  }
+  updateChart(x);
+  }
+  //===============================================DOGE END==================================================
+  
+} 
